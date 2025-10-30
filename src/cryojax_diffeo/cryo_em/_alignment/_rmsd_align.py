2a1c580a5deb6d1dbcaaced4688a3083a57042d1
DSilva27 <david.silvas@udea.edu.co>
2025-10-30 19:01:52 -0400

    implmenet rmsd guided diffusion module

--- src/cryojax_diffeo/cryo_em/_alignment/_rmsd_align.py
+++ src/cryojax_diffeo/cryo_em/_alignment/_rmsd_align.py
@@ -1,3 +1,4 @@
 """
-Might not need this module, if needed we can just use mdtraj or write our own implementation of Kabsch-Ubeyama
+Might not need this module, if needed we can just use mdtraj
+or write our own implementation of Kabsch-Ubeyama
 """
